2f8d641291c038bb0402c5b5f60522f39410dce9
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 21:12:53 +0300

    Решил задачу №1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа 
+
+int ran = new Random().Next(10,100);    //Второе числа на 1 больше
+System.Console.WriteLine(ran);
+
+int first = ran / 10;
+int second = ran % 10;
+
+if(first > second)
+{
+    System.Console.WriteLine(first);
+}
+else
+{
+    System.Console.WriteLine(second);
+}
