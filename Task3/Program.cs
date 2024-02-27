f94b9a7fcf5d870253e56c8f0b82645af7e6b376
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 22:44:02 +0300

    Решил задачу №3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа, которая принимает 2 числа и выводит
+//является ли второе число кратно первому.
+//В противном случае выводит остаток
+
+System.Console.WriteLine("Введите первое число:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число:");
+int x2 = Convert.ToInt32(Console.ReadLine());                        //аналог Convert.ToInt32 = int.Parse
+int ost = x2 % x1;
+if (ost == 0)
+{
+    System.Console.WriteLine("Число " + x2 + " кратно " + x1);       //Конкатенация
+}
+else
+{
+    System.Console.WriteLine($"Число {x2} не кратно {x1}");          //Интерполяция
+    System.Console.WriteLine("Остаток от деления = " + ost);
+}
