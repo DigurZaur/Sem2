dc425969221c8570242d685c4cd46a943e45676e
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 22:29:57 +0300

    Решил задачу №2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая выводит случайное
+// трехзначное число и удаляет вторую цифру
+
+int num = new Random().Next(100, 1000);
+System.Console.WriteLine(num);
+
+int first = num / 100 * 10;
+int third = num % 10;
+int result = (first + third);
+System.Console.WriteLine(result);
--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+157305c9f3d7897a29b7ac5ebb746fe6aed467d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
